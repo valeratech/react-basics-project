51aa1b8e870b778f31a29ee91b5571daed254e34
Ryan Valera <valeraryan@gmail.com>
2023-11-01 11:15:31 -0700

    Modify the Header component to dynamically change the page description using the genRandomInt and reactDescriptions array. Complete header by uploading images to the assets folder for import.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import reactAtomImg from './assets/react-core-concepts.png';
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
@@ -5,12 +7,13 @@ function genRandomInt(max) {
 }
 
 function Header() {
+    const description = reactDescriptions[genRandomInt(reactDescriptions.length)];
     return (
         <header>
-            <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+            <img src={reactAtomImg} alt="Stylized atom" />
             <h1>React Essentials</h1>
             <p>
-                Fundamental React concepts you will need for almost any app you are going to build!
+                {description} React concepts you will need for almost any app you are going to build!
             </p>
         </header>
     );
